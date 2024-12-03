638ba06be5f841786c756e229304f41de0823039
unknown <politia444@gmail.com>
2024-12-03 23:36:57 +0900

    로그인 창

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,22 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.SignInButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.button1.Font = new System.Drawing.Font("굴림", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(288, 207);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 122);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "출력";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.ButtonClick);
             // 
             // label1
             // 
@@ -51,16 +40,47 @@
             this.label1.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label1.Location = new System.Drawing.Point(292, 120);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 27);
+            this.label1.Size = new System.Drawing.Size(124, 27);
             this.label1.TabIndex = 1;
+            this.label1.Text = "로그인창";
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(297, 194);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(119, 21);
+            this.id.TabIndex = 2;
+            this.id.Text = "아이디";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(297, 251);
+            this.password.Name = "password";
+            this.password.PasswordChar = '＊';
+            this.password.Size = new System.Drawing.Size(119, 21);
+            this.password.TabIndex = 3;
+            this.password.Text = "패스워드";
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.SignInButton.Location = new System.Drawing.Point(297, 299);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(119, 23);
+            this.SignInButton.TabIndex = 4;
+            this.SignInButton.Text = "로그인";
+            this.SignInButton.UseVisualStyleBackColor = false;
+            this.SignInButton.Click += new System.EventHandler(this.SingInButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SignInButton);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.id);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -69,9 +89,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button SignInButton;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -9,9 +9,23 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void ButtonClick(object sender, System.EventArgs e)
+        //private void ButtonClick(object sender, System.EventArgs e)
+        //{
+        //    label1.Text = "Hello World!";
+        //}
+
+        // 로그인 창
+        private void SingInButton_Click(object sender, System.EventArgs e)
         {
-            label1.Text = "Hello World!";
+            string userId = id.Text;
+            string userPassword = password.Text;
+
+            if(userId.Equals("JMD") && userPassword.Equals("1")){
+                MessageBox.Show("로그인에 성공했습니다.", "로그인");
+            } else
+            {
+                MessageBox.Show("로그인에 실패했습니다.", "로그인");
+            }
         }
     }
 }
